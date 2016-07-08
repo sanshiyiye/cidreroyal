c6b8465a2a05c81f810d397452ca7744f79eb075
gaojunohyeah <gaojunohyeah@gmail.com>
2016-07-08 19:14:37 +0800

    翻译和eslint配置

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+"use strict";
+
+module.exports = {
+  "env": {
+    "browser": true,
+    "node": true
+  },
+  "globals": {
+  },
+  "rules": {
+    "semi": [2, "always"],
+    "camelcase": 2,
+    "curly": 2,
+  },
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module",
+    "ecmaFeatures": {
+    }
+  }
+};
